3fe13a329728252d90992e875903c7c24beb07b7
Songmw <1397472492@qq.com>
2023-01-31 19:47:11 +0800

    feat:完善项目全部IDL文件

--- idl/api.thrift
+++ idl/api.thrift
@@ -1,5 +1,6 @@
 include "base.thrift"
 include "interaction.thrift"
+include "social.thrift"
 
 namespace go douyin.api
 
@@ -17,4 +18,12 @@ service DouyinApi {
     interaction.douyin_favorite_list_response FavoriteList(1:interaction.douyin_favorite_list_request req)(api.get="/douyin/favorite/list")
     interaction.douyin_comment_action_response CommentAction(1:interaction.douyin_comment_action_request req)(api.post="/douyin/comment/action")
     interaction.douyin_comment_list_response CommentList(1:interaction.douyin_comment_list_request req)(api.get="/douyin/comment/list")
+
+    // social service
+    social.douyin_follow_action_response FollowAction(1:social.douyin_follow_action_request req)(api.get="/douyin/relation/action")
+    social.douyin_following_list_response FollowList(1:social.douyin_following_list_request req)(api.get="/douyin/relatioin/follow/list")
+    social.douyin_follower_list_response FollowerList(1:social.douyin_follower_list_request req)(api.get="/douyin/relation/follower/list")
+    social.douyin_relation_friend_list_response FriendList(1:social.douyin_relation_friend_list_request req)(api.get="/douyin/relation/friend/list")
+    social.douyin_message_list_response MessageList(1:social.douyin_message_list_request req)(api.get="/douyin/message/list")
+    social.douyin_message_action_response SendMessage(1:social.douyin_message_action_request req)(api.get="/douyin/message/action")
 }
--- idl/model.thrift
+++ idl/model.thrift
@@ -8,6 +8,12 @@ struct User {
     5:required bool is_follow
 }
 
+struct FriendUser {
+    1: User user
+    2: optional string message // 和该好友的最新聊天记录
+    3: required i64 msgType    // 0-当前用户接收的消息， 1-当前用户发送的消息
+}
+
 struct Video {
     1:required i64 id
     2:required User author
@@ -29,4 +35,13 @@ struct Comment {
 struct BaseReq {
     1:optional i64 user_id
     2:optional string username
+}
+
+// 消息
+struct Message {
+    1: required i64 id             // 消息id
+    2: required i64 to_user_id     // 消息接收者
+    3: required i64 from_user_id   // 消息发送者
+    4: required string content     // 消息内容
+    5: optional string create_time // 消息创建时间
 }
