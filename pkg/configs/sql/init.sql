b00692662fbce8b9e28a6d7702dd10e488cb87ad
Yogen <1654622146@qq.com>
2023-02-01 19:25:08 +0800

    relation add action

--- pkg/configs/sql/init.sql
+++ pkg/configs/sql/init.sql
@@ -1,53 +1,69 @@
-CREATE TABLE `user`
-(
-    id         int(11) auto_increment primary key,
-    username   varchar(31)  not null,
-    password   varchar(255) not null,
-    created_at datetime,
-    updated_at datetime,
-    deleted_at datetime
-) engine InnoDB
-  default charset utf8mb4;
-
-DROP TABLE IF EXISTS `comment`;
-CREATE TABLE `comment` (
-                           `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-                           `created_at` datetime(3) DEFAULT NULL,
-                           `updated_at` datetime(3) DEFAULT NULL,
-                           `deleted_at` datetime(3) DEFAULT NULL,
-                           `vid` bigint(20) unsigned NOT NULL,
-                           `uid` bigint(20) unsigned NOT NULL,
-                           `content` longtext COLLATE utf8mb4_bin NOT NULL,
-                           PRIMARY KEY (`id`),
-                           KEY `idx_comment_deleted_at` (`deleted_at`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
-DROP TABLE IF EXISTS `relation`;
-CREATE TABLE `relation` (
-                            `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-                            `created_at` datetime(3) DEFAULT NULL,
-                            `updated_at` datetime(3) DEFAULT NULL,
-                            `deleted_at` datetime(3) DEFAULT NULL,
-                            `concerner_id` bigint(20) unsigned NOT NULL,
-                            `concerned_id` bigint(20) unsigned NOT NULL,
-                            PRIMARY KEY (`id`),
-                            KEY `idx_relation_deleted_at` (`deleted_at`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
-
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-                        `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+                        `id` bigint unsigned NOT NULL AUTO_INCREMENT,
                         `created_at` datetime(3) DEFAULT NULL,
                         `updated_at` datetime(3) DEFAULT NULL,
                         `deleted_at` datetime(3) DEFAULT NULL,
-                        `username` longtext COLLATE utf8mb4_bin NOT NULL,
-                        `password` longtext COLLATE utf8mb4_bin NOT NULL,
+                        `username` varchar(20) NOT NULL,
+                        `password` varchar(255) NOT NULL,
                         `follow_count` bigint(20) unsigned DEFAULT '0',
                         `follower_count` bigint(20) unsigned DEFAULT '0',
                         PRIMARY KEY (`id`),
                         KEY `idx_user_deleted_at` (`deleted_at`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE `message` (
+                           `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+                           `created_at` datetime(3) DEFAULT NULL,
+                           `updated_at` datetime(3) DEFAULT NULL,
+                           `deleted_at` datetime(3) DEFAULT NULL,
+                           `uid` bigint unsigned DEFAULT NULL,
+                           `to_uid` bigint unsigned DEFAULT NULL,
+                           `content` blob,
+                           PRIMARY KEY (`id`),
+                           KEY `idx_message_deleted_at` (`deleted_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+DROP TABLE IF EXISTS `likes`;
+CREATE TABLE `likes` (
+                         `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+                         `created_at` datetime(3) DEFAULT NULL,
+                         `updated_at` datetime(3) DEFAULT NULL,
+                         `deleted_at` datetime(3) DEFAULT NULL,
+                         `uid` bigint unsigned DEFAULT NULL,
+                         `vid` bigint unsigned DEFAULT NULL,
+                         `action` tinyint(1) DEFAULT '0',
+                         PRIMARY KEY (`id`),
+                         KEY `idx_likes_deleted_at` (`deleted_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment` (
+                           `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+                           `created_at` DATETIME(3) DEFAULT NULL,
+                           `updated_at` DATETIME(3) DEFAULT NULL,
+                           `deleted_at` DATETIME(3) DEFAULT NULL,
+                           `vid` BIGINT(20) UNSIGNED NOT NULL,
+                           `uid` BIGINT(20) UNSIGNED NOT NULL,
+                           `content` LONGTEXT COLLATE utf8mb4_bin NOT NULL,
+                           PRIMARY KEY (`id`),
+                           KEY `idx_comment_deleted_at` (`deleted_at`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+DROP TABLE IF EXISTS `relation`;
+CREATE TABLE `relation` (
+                            `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+                            `created_at` DATETIME(3) DEFAULT NULL,
+                            `updated_at` DATETIME(3) DEFAULT NULL,
+                            `deleted_at` DATETIME(3) DEFAULT NULL,
+                            `concerner_id` BIGINT(20) UNSIGNED NOT NULL,
+                            `concerned_id` BIGINT(20) UNSIGNED NOT NULL,
+                            `action` tinyint(1) DEFAULT '0',
+                            PRIMARY KEY (`id`),
+                            KEY `idx_relation_deleted_at` (`deleted_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 DROP TABLE IF EXISTS `video`;
 CREATE TABLE `video` (
@@ -62,5 +78,5 @@ CREATE TABLE `video` (
                          `title` longtext COLLATE utf8mb4_bin NOT NULL,
                          `uid` bigint(20) unsigned NOT NULL,
                          PRIMARY KEY (`id`),
-                         KEY `idx_vedio_deleted_at` (`deleted_at`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+                         KEY `idx_video_deleted_at` (`deleted_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
