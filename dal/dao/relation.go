2c1bb39898c59d21a190f28466ead895336ffbfd
herocbn <m202170730@hust.edu.cn>
2023-02-10 16:54:49 +0800

    登录用户查看用户信息修改

--- dal/dao/relation.go
+++ dal/dao/relation.go
@@ -15,8 +15,7 @@ type Relation struct {
 
 // IsUserFollowed 两个用户有是否关注 输入两个用户的Id a->b
 func (r *Relation) IsUserFollowed(ctx context.Context, concernerID int64, concernedID int64) (isFollow bool, err error) {
-	relation := model.Relation{}
-	if err = db.WithContext(ctx).Model(model.Relation{}).Select("action").Where("concerner_id = ? AND concerned_id = ?", concernerID, concernedID).First(&relation).Error; err != nil {
+	if err = db.WithContext(ctx).Model(model.Relation{}).Select("action").Where("concerner_id = ? AND concerned_id = ?", concernerID, concernedID).First(&isFollow).Error; err != nil {
 		if err == gorm.ErrRecordNotFound {
 			// relation 中不存在关注的关系，也可表示未关注
 			Log.Infof("IsUserFollowed err：%v, concerner_id: %d AND concerned_id：%d", err, concernerID, concernedID)
@@ -27,13 +26,13 @@ func (r *Relation) IsUserFollowed(ctx context.Context, concernerID int64, concer
 			return false, err
 		}
 	}
-	return relation.Action, nil
+	return
 }
 
 // FollowListLen 获取关注的数目，输入用户id，输出用户关注的人数量的信息和报错信息
 func (r *Relation) FollowListLen(ctx context.Context, id int64) (nums *int64, err error) {
 	var len int64
-	err = db.WithContext(ctx).Model(model.Relation{}).Where("concerner_id=?", id).Count(&len).Error
+	err = db.WithContext(ctx).Model(model.Relation{}).Where("concerner_id=? AND action = 1", id).Count(&len).Error
 	if err != nil {
 		Log.Errorf("get concerner-nums err: %v, userId: %+v", err, id)
 		return
@@ -46,7 +45,7 @@ func (r *Relation) FollowListLen(ctx context.Context, id int64) (nums *int64, er
 // FollowerListLen 获取粉丝数目，输入用户id，输出用户的粉丝数目和报错信息
 func (r *Relation) FollowerListLen(ctx context.Context, id int64) (nums *int64, err error) {
 	var len int64
-	err = db.WithContext(ctx).Model(model.Relation{}).Where("concerned_id=?", id).Count(&len).Error
+	err = db.WithContext(ctx).Model(model.Relation{}).Where("concerned_id=? AND action = 1", id).Count(&len).Error
 	if err != nil {
 		Log.Errorf("get concerned-nums err: %v, userId: %+v", err, id)
 		return
