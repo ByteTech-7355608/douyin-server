e5af358ec9e3406b4cd294a8d981bafc4ace61a7
Songmw <1397472492@qq.com>
2023-02-11 21:06:49 +0800

    <fix>:修改响应码201-->4001

--- pkg/constants/error.go
+++ pkg/constants/error.go
@@ -17,11 +17,11 @@ func (s *RespStatus) Errormsg() *string {
 }
 
 var (
-	ErrUserExist       = &RespStatus{201, errors.New("用户名已存在")}
-	ErrUserNotExist    = &RespStatus{202, errors.New("用户名不存在")}
-	ErrInvalidPassword = &RespStatus{203, errors.New("用户密码错误")}
-	ErrTokenExpires    = &RespStatus{204, errors.New("token已过期")}
-	ErrNotLogin        = &RespStatus{205, errors.New("未登录")}
-	ErrInvalidAuth     = &RespStatus{206, errors.New("认证格式有误")}
-	ErrInvalidToken    = &RespStatus{207, errors.New("无效的Token")}
+	ErrUserExist       = &RespStatus{4001, errors.New("用户名已存在")}
+	ErrUserNotExist    = &RespStatus{4002, errors.New("用户名不存在")}
+	ErrInvalidPassword = &RespStatus{4003, errors.New("用户密码错误")}
+	ErrTokenExpires    = &RespStatus{4004, errors.New("token已过期")}
+	ErrNotLogin        = &RespStatus{4005, errors.New("未登录")}
+	ErrInvalidAuth     = &RespStatus{4006, errors.New("认证格式有误")}
+	ErrInvalidToken    = &RespStatus{4007, errors.New("无效的Token")}
 )
