30215be8f1a4a64b062134a5b25120bee7a54f65
herocbn <m202170730@hust.edu.cn>
2023-02-12 22:17:14 +0800

    用户关注vs1

--- cmd/api/biz/handler/social.go
+++ cmd/api/biz/handler/social.go
@@ -8,34 +8,23 @@ import (
 	"github.com/cloudwego/hertz/pkg/app"
 )
 
-// FollowAction .
+// RelationAction .
 // @router /douyin/relation/action [POST]
-func (h *Handler) FollowAction(ctx context.Context, c *app.RequestContext) {
-	req := &api.DouyinFollowActionRequest{}
-	rpcReq := &rpc.DouyinFollowActionRequest{}
-	if h.Pre(ctx, c, req, rpcReq) {
-		rpcReq.BaseReq = h.GetReqBase(c)
-		rpcResp, err := h.RPC().Social().Client().FollowAction(ctx, rpcReq)
+func (h *Handler) RelationAction(ctx context.Context, c *app.RequestContext) {
+	req := api.DouyinFollowActionRequest{}
+	rpcReq := rpc.DouyinFollowActionRequest{}
+	if h.Pre(ctx, c, &req, &rpcReq) {
+		userID, ok := c.Get("userid") // 当前用户id
+		if !ok {
+			return
+		}
+		rpcReq.FollowingId = userID.(int64) //关注者的id
+		rpcReq.FollowerId = req.FollowerID  //被关注者的id
+		rpcResp, err := h.RPC().Social().Client().FollowAction(ctx, &rpcReq)
 		if err != nil {
 			return
 		}
-		resp := api.DouyinFollowActionResponse{}
-		h.After(ctx, c, &resp, rpcResp, err)
-	}
-}
-
-// FollowList .
-// @router /douyin/relation/list [GET]
-func (h *Handler) FollowList(ctx context.Context, c *app.RequestContext) {
-	req := &api.DouyinFollowingListRequest{}
-	rpcReq := &rpc.DouyinFollowingListRequest{}
-	if h.Pre(ctx, c, req, rpcReq) {
-		//rpcReq.BaseReq = h.GetReqBase(c)
-		rpcResp, err := h.RPC().Social().Client().FollowList(ctx, rpcReq)
-		if err != nil {
-			return
-		}
-		resp := api.DouyinFollowingListResponse{}
-		h.After(ctx, c, &resp, rpcResp, err)
+		resp := rpc.DouyinFollowActionResponse{}
+		h.After(ctx, c, &resp, &rpcResp, err)
 	}
 }
