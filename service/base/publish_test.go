c8be5289861264e6c71d882923c8654cd74cbab8
zyj <1654622146@qq.com>
2023-02-12 11:12:40 +0800

    publish action fix db

--- service/base/publish_test.go
+++ service/base/publish_test.go
@@ -51,7 +51,7 @@ var _ = Describe("Publish Test", func() {
 			req := base2.NewDouyinPublishActionRequest()
 			req.Title = "title_test"
 			req.Data = []byte{'a', 'b', 'c'}
-			req.Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6Inp5ajMiLCJpc3MiOiJkb3V5aW4tc2VydmljZSIsImV4cCI6MTY3NjEyNDI0OX0.DARtgkOMzzuTr0YRjvFUiuJPu2-tqRE4t0xv1UMNJ8I"
+			req.Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6Inp5ajMiLCJpc3MiOiJkb3V5aW4tc2VydmljZSIsImV4cCI6MTY3NjE3NTA0OX0.GWFgC7NlorrIphPBsBzt0aaaWmCU8HG2zO3em5h301s"
 			resp, err := svc.PublishAction(ctx, req)
 			Expect(err).To(BeNil())
 			Expect(resp.StatusCode).To(Equal(int32(200)))
