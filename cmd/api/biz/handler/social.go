c8229e75f9344c5281db820c7269f43032635f1b
herocbn <m202170730@hust.edu.cn>
2023-02-16 13:28:36 +0800

    关注操作

--- cmd/api/biz/handler/social.go
+++ cmd/api/biz/handler/social.go
@@ -1,1 +1,25 @@
 package handler
+
+import (
+	api "ByteTech-7355608/douyin-server/cmd/api/biz/model/douyin/social"
+	rpc "ByteTech-7355608/douyin-server/kitex_gen/douyin/social"
+	"context"
+
+	"github.com/cloudwego/hertz/pkg/app"
+)
+
+// RelationAction .
+// @router /douyin/relation/action [POST]
+func (h *Handler) FollowAction(ctx context.Context, c *app.RequestContext) {
+	req := api.DouyinFollowActionRequest{}
+	rpcReq := rpc.DouyinFollowActionRequest{}
+	if h.Pre(ctx, c, &req, &rpcReq) {
+		rpcReq.BaseReq = h.GetReqBase(c)
+		rpcResp, err := h.RPC().Social().Client().FollowAction(ctx, &rpcReq)
+		if err != nil {
+			return
+		}
+		resp := rpc.DouyinFollowActionResponse{}
+		h.After(ctx, c, &resp, &rpcResp, err)
+	}
+}
