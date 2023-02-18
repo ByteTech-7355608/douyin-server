30de1992c78bdcdbd2a3e478c6f7f6eca76fb175
SouthWind6510 <z1113270382@gmail.com>
2023-02-18 23:40:08 +0800

    feat: 引入redis作为缓存

--- service/interaction/init.go
+++ service/interaction/init.go
@@ -1,18 +1,21 @@
 package interaction
 
 import (
+	"ByteTech-7355608/douyin-server/dal/cache"
 	"ByteTech-7355608/douyin-server/dal/dao"
 	"ByteTech-7355608/douyin-server/rpc"
 )
 
 type Service struct {
-	dao *dao.Dao
-	rpc *rpc.RPC
+	dao   *dao.Dao
+	rpc   *rpc.RPC
+	cache *cache.RedisCache
 }
 
 func NewService(rpc *rpc.RPC) *Service {
 	return &Service{
-		dao: dao.NewDao(),
-		rpc: rpc,
+		dao:   dao.NewDao(),
+		rpc:   rpc,
+		cache: cache.NewRedisCache(),
 	}
 }
