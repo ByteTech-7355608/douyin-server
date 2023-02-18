4397d8388ffc2f38695f10e630e7d3a5985ff766
herocbn <m202170730@hust.edu.cn>
2023-02-18 17:34:55 +0800

    用户关注列表反射添加

---
+++ script/set_up.sh
@@ -0,0 +1,17 @@
+#! bin/bash
+if [ $2=="start" ];then
+echo "begin~"
+sh ./run_api.sh >api.log  &
+echo "$!" > pid
+sh ./run_base.sh >base.log  & 
+echo "$!" > pid
+sh ./run_interaction.sh >interaction.log  & 
+echo "$!" > pid
+sh ./run_social.sh >social.log  &
+echo "$!" > pid
+elif [ $2=="stop" ];then
+    kill `cat pid`
+    echo "finished~"
+else
+echo "input correct condition"
+fi
