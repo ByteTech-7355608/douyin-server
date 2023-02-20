5e647df020cee94d01b3b3a33204b23e54523cd3
Sunxy <714596429@qq.com>
2023-02-21 03:14:32 +0800

    v1

--- service/social/relation.go
+++ service/social/relation.go
@@ -1,23 +1,149 @@
 package social
 
 import (
-	"ByteTech-7355608/douyin-server/kitex_gen/douyin/model"
+	"ByteTech-7355608/douyin-server/dal/cache"
+	"ByteTech-7355608/douyin-server/dal/dao/model"
+	"strconv"
 
 	model2 "ByteTech-7355608/douyin-server/kitex_gen/douyin/model"
 
 	"ByteTech-7355608/douyin-server/kitex_gen/douyin/social"
 	. "ByteTech-7355608/douyin-server/pkg/configs"
+	"ByteTech-7355608/douyin-server/pkg/constants"
 	"context"
 )
 
-const (
-	KAddType    = 1
-	KDeleteType = 2
-)
+func (s *Service) FollowAction(ctx context.Context, req *social.DouyinFollowActionRequest) (resp *social.DouyinFollowActionResponse, err error) {
+	resp = social.NewDouyinFollowActionResponse()
+	from_id, to_id := req.GetBaseReq().GetUserId(), req.GetToUserId()
+
+	// 1. 判断需要操作的对象在缓存中是否存在
+	if s.cache.Relation.IsExists(ctx, from_id) == 0 {
+		// 缓存中不存在用户粉丝列表
+		userList, err := s.dao.Relation.FollowList(ctx, from_id)
+		if err != nil {
+			Log.Errorf("get follow list err: %v, uid: %v", err, from_id)
+		}
+		if len(userList) > 0 {
+			kv := make([]string, 0)
+			for _, user := range userList {
+				kv = append(kv, strconv.FormatInt(user.ID, 10))
+				kv = append(kv, "1")
+			}
+			if !s.cache.Relation.SetFollowList(ctx, from_id, kv...) {
+				Log.Errorf("set follow list to redis err")
+				return resp, constants.ErrWriteCache
+			}
+		}
+	}
+
+	if s.cache.User.IsExists(ctx, from_id) == 0 {
+		// 当关注的用户不存在
+		user, err := s.dao.User.QueryUser(ctx, from_id)
+		if err != nil {
+			Log.Errorf("query user %v err: %v", from_id, err)
+			return resp, err
+		}
+		userModel := &cache.UserModel{
+			Id:              user.ID,
+			Name:            user.Username,
+			FollowCount:     user.FollowCount,
+			FollowerCount:   user.FollowerCount,
+			Avatar:          user.Avatar,
+			BackgroundImage: user.BackgroundImage,
+			Signature:       user.Signature,
+			TotalFavorited:  user.TotalFavorited,
+			WorkCount:       user.WorkCount,
+			FavoriteCount:   user.FavoriteCount,
+		}
+		if !s.cache.User.SetUserMessage(ctx, userModel) {
+			Log.Errorf("set user message to redis err: %v", err)
+			return resp, constants.ErrWriteCache
+		}
+	}
+
+	if s.cache.User.IsExists(ctx, to_id) == 0 {
+		// 当被关注的用户不存在
+		user, err := s.dao.User.QueryUser(ctx, to_id)
+		if err != nil {
+			Log.Errorf("query user %v err: %v", to_id, err)
+			return resp, err
+		}
+		userModel := &cache.UserModel{
+			Id:              user.ID,
+			Name:            user.Username,
+			FollowCount:     user.FollowCount,
+			FollowerCount:   user.FollowerCount,
+			Avatar:          user.Avatar,
+			BackgroundImage: user.BackgroundImage,
+			Signature:       user.Signature,
+			TotalFavorited:  user.TotalFavorited,
+			WorkCount:       user.WorkCount,
+			FavoriteCount:   user.FavoriteCount,
+		}
+		if !s.cache.User.SetUserMessage(ctx, userModel) {
+			Log.Errorf("set user message to redis err: %v", err)
+			return resp, constants.ErrWriteCache
+		}
+	}
+
+	// 2. 判断操作类型
+	var action int64
+	follow := s.cache.Relation.IsFollow(ctx, from_id, to_id)
+	if follow && req.GetActionType() == 2 {
+		// 已关注并取消关注
+		action = -1
+	} else if !follow && req.GetActionType() == 1 {
+		// 关注
+		action = 1
+	} else {
+		// 关系和操作不匹配
+		return resp, constants.ErrUpdateRecord
+	}
+
+	// 3. 原子性的进行操作
+	// 更新用户关注列表
+	if !s.cache.Relation.FollowAction(ctx, from_id, to_id, action) {
+		return resp, constants.ErrWriteCache
+	}
+	// 更新用户关注数量
+	if !s.cache.User.IncrUserField(ctx, from_id, "follow_count", action) {
+		return resp, constants.ErrWriteCache
+	}
+	// 更新用户被关注数量
+	if !s.cache.User.IncrUserField(ctx, from_id, "follower_count", action) {
+		return resp, constants.ErrWriteCache
+	}
+
+	return
+}
 
 func (s *Service) FollowList(ctx context.Context, req *social.DouyinFollowingListRequest) (resp *social.DouyinFollowingListResponse, err error) {
 	resp = social.NewDouyinFollowingListResponse()
-	userID := req.GetBaseReq().GetUserId()
+	user_id, from_id := req.GetBaseReq().GetUserId(), req.GetUserId()
+
+	// 1. 判断需要操作的对象在缓存中是否存在
+	var userList []*model.User
+	if s.cache.Relation.IsExists(ctx, from_id) == 0 {
+		// 缓存中不存在用户粉丝列表
+		userList, err = s.dao.Relation.FollowList(ctx, from_id)
+		if err != nil {
+			Log.Errorf("get follow list err: %v, uid: %v", err, from_id)
+		}
+		if len(userList) > 0 {
+			kv := make([]string, 0)
+			for _, user := range userList {
+				kv = append(kv, strconv.FormatInt(user.ID, 10))
+				kv = append(kv, "1")
+			}
+			if !s.cache.Relation.SetFollowList(ctx, from_id, kv...) {
+				Log.Errorf("set follow list to redis err")
+				return resp, constants.ErrWriteCache
+			}
+		}
+	} else {
+		// 缓存中存在用户粉丝列表
+	}
 
 	list, err := s.dao.Relation.FollowList(ctx, req.GetUserId())
 	if err != nil {
@@ -58,7 +184,7 @@ func (s *Service) FollowerList(ctx context.Context, req *social.DouyinFollowerLi
 		return nil, err
 	}
 
-	var followers []*model.User
+	var followers []*model2.User
 	for _, followerid := range followeridlist {
 		userInstance, err := s.dao.User.FindUserById(ctx, followerid)
 		if err != nil {
@@ -72,7 +198,7 @@ func (s *Service) FollowerList(ctx context.Context, req *social.DouyinFollowerLi
 			continue
 		}
 
-		user := &model.User{
+		user := &model2.User{
 			Id:            userInstance.ID,
 			Name:          userInstance.Username,
 			FollowCount:   &userInstance.FollowCount,
@@ -96,7 +222,7 @@ func (s *Service) FriendList(ctx context.Context, req *social.DouyinRelationFrie
 		return
 	}
 
-	var friends []*model.FriendUser
+	var friends []*model2.FriendUser
 	for _, followerid := range followeridlist {
 		userInstance, err := s.dao.User.FindUserById(ctx, followerid)
 		if err != nil {
@@ -124,7 +250,7 @@ func (s *Service) FriendList(ctx context.Context, req *social.DouyinRelationFrie
 
 		// 互相没有发送过信息
 		if msg.ID == 0 {
-			friend := &model.FriendUser{
+			friend := &model2.FriendUser{
 				Id:            userInstance.ID,
 				Name:          userInstance.Username,
 				FollowCount:   &userInstance.FollowCount,
@@ -144,7 +270,7 @@ func (s *Service) FriendList(ctx context.Context, req *social.DouyinRelationFrie
 			msgType = 0
 		}
 
-		friend := &model.FriendUser{
+		friend := &model2.FriendUser{
 			Id:            userInstance.ID,
 			Name:          userInstance.Username,
 			FollowCount:   &userInstance.FollowCount,
@@ -160,44 +286,4 @@ func (s *Service) FriendList(ctx context.Context, req *social.DouyinRelationFrie
 
 	resp.SetUserList(friends)
 	return resp, nil
-}
-
-func (s *Service) FollowAction(ctx context.Context, req *social.DouyinFollowActionRequest) (resp *social.DouyinFollowActionResponse, err error) {
-	resp = social.NewDouyinFollowActionResponse()
-	concerner_id := *req.BaseReq.UserId
-	concerned_id := req.ToUserId
-	flag, record, err := s.dao.Relation.CheckRecord(ctx, concerner_id, concerned_id)
-	if err != nil {
-		Log.Errorf("check relation err: %v", err)
-		return nil, err
-	}
-	switch req.ActionType {
-	case KAddType:
-		if !flag {
-			//没有此记录
-			err = s.dao.Relation.AddRelation(ctx, concerner_id, concerned_id)
-			if err != nil {
-				Log.Errorf("add relation err: %v", err)
-				return nil, err
-			}
-		} else {
-			//存在该记录
-			err = s.dao.Relation.UpdatedRelation(ctx, record, 1)
-			if err != nil {
-				Log.Errorf("update relation err: %v", err)
-				return nil, err
-			}
-
-		}
-
-	case KDeleteType:
-		if flag {
-			err = s.dao.Relation.UpdatedRelation(ctx, record, 0)
-			if err != nil {
-				Log.Errorf("update relation err: %v", err)
-				return nil, err
-			}
-		}
-	}
-	return
 }
