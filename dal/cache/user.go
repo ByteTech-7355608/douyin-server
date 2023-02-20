d3babd69ec597e4436bf4b9615146ee44635a2c4
zyj <1654622146@qq.com>
2023-02-20 22:13:00 +0800

    format code

--- dal/cache/user.go
+++ dal/cache/user.go
@@ -77,11 +77,11 @@ func UserModel2User(userModel *UserModel) *model.User {
 
 func Map2UserModel(mp map[string]string) (userModel *UserModel, err error) {
 	var id, followCount, followerCount, totalFavorited, workCount, favoriteCount int64
-	id, err = strconv.ParseInt(mp["id"], 10, 64)                              //nolint: staticcheck
-	followCount, err = strconv.ParseInt(mp["follow_count"], 10, 64)           //nolint: staticcheck
-	followerCount, err = strconv.ParseInt(mp["follower_count"], 10, 64)       //nolint: staticcheck
+	id, err = strconv.ParseInt(mp["id"], 10, 64)                           //nolint: staticcheck
+	followCount, err = strconv.ParseInt(mp["follow_count"], 10, 64)        //nolint: staticcheck
+	followerCount, err = strconv.ParseInt(mp["follower_count"], 10, 64)    //nolint: staticcheck
 	totalFavorited, err = strconv.ParseInt(mp["total_favorited	"], 10, 64) //nolint: staticcheck
-	workCount, err = strconv.ParseInt(mp["work_count"], 10, 64)               //nolint: staticcheck
+	workCount, err = strconv.ParseInt(mp["work_count"], 10, 64)            //nolint: staticcheck
 	favoriteCount, err = strconv.ParseInt(mp["favorite_count"], 10, 64)
 	if err != nil {
 		Log.Errorf("parse int from map err: %v", err)
