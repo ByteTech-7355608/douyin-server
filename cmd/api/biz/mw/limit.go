fcd219e7638834c0fef3d32f4cb2d7ffbde90832
zyj <1654622146@qq.com>
2023-02-21 10:15:52 +0800

    fix redis

--- cmd/api/biz/mw/limit.go
+++ cmd/api/biz/mw/limit.go
@@ -5,7 +5,6 @@ import (
 	"ByteTech-7355608/douyin-server/dal/cache"
 	"ByteTech-7355608/douyin-server/pkg/constants"
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/cloudwego/hertz/pkg/app"
@@ -17,14 +16,11 @@ func IPLimitMiddleware() app.HandlerFunc {
 		key := constants.GetFavoriteLmtKey(clientip)
 		cache.NewRedisCache()
 		if cache.Exists(ctx, key) == 0 {
-			fmt.Println("2")
 			// redis中不存在当前key
 			cache.Set(ctx, key, 1, time.Minute)
 		} else {
-			fmt.Println("3")
 			cnt, err := cache.Incr(ctx, key)
 			if err != nil {
-				fmt.Println("limite,err:", err)
 				handler.Response(ctx, c, constants.ErrWriteCache)
 				c.Abort()
 				return
