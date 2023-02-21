3f3236daa3a570a91c732e9858db1114778e7e27
herocbn <m202170730@hust.edu.cn>
2023-02-21 10:32:22 +0800

    用户信息缓存，单测待补

--- service/base/user.go
+++ service/base/user.go
@@ -1,6 +1,7 @@
 package base
 
 import (
+	"ByteTech-7355608/douyin-server/dal/cache"
 	"ByteTech-7355608/douyin-server/kitex_gen/douyin/base"
 	model2 "ByteTech-7355608/douyin-server/kitex_gen/douyin/model"
 	. "ByteTech-7355608/douyin-server/pkg/configs"
@@ -40,24 +41,46 @@ func (s *Service) UserMsg(ctx context.Context, req *base.DouyinUserRequest) (res
 	resp = base.NewDouyinUserResponse()
 	var user model2.User
 	user.Id = req.UserId //被查看的用户id
-	userinfo, err := s.dao.User.FindUserById(ctx, user.Id)
-	if err != nil {
-		Log.Errorf("Get user err: %v", err)
-		return
+	if s.cache.User.IsExists(ctx, user.Id) != 0 {
+		usermsg, err := s.cache.User.GetUserMessage(ctx, user.Id)
+		if err != nil {
+			Log.Errorf("Get usermsg from redis err: %v", err)
+			return resp, err
+		}
+		user = *cache.UserModel2User(usermsg)
+	} else {
+		userinfo, err := s.dao.User.FindUserById(ctx, user.Id)
+		if err != nil {
+			Log.Errorf("Get usermsg from db err: %v", err)
+			return resp, err
+		}
+		user.Name = userinfo.Username
+		user.FollowCount = &userinfo.FollowCount
+		user.FollowerCount = &userinfo.FollowerCount
+		user.FavoriteCount = &userinfo.FavoriteCount
+		user.TotalFavorited = &userinfo.TotalFavorited
+		user.WorkCount = &userinfo.WorkCount
+		user.Avatar = &userinfo.Avatar
+		user.BackgroundImage = &userinfo.BackgroundImage
+		user.Signature = &userinfo.Signature
+		cache_user := cache.User2UserModel(&user)
+		s.cache.User.SetUserMessage(ctx, cache_user)
 	}
-	user.Name = userinfo.Username
-	user.FollowCount = &userinfo.FollowCount
-	user.FollowerCount = &userinfo.FollowerCount
-	user.FavoriteCount = &userinfo.FavoriteCount
-	user.TotalFavorited = &userinfo.TotalFavorited
-	user.WorkCount = &userinfo.WorkCount
-	user.Avatar = &userinfo.Avatar
-	user.BackgroundImage = &userinfo.BackgroundImage
-	user.Signature = &userinfo.Signature
-	user.IsFollow, err = s.dao.Relation.IsUserFollowed(ctx, *req.BaseReq.UserId, user.Id)
-	if err != nil {
-		Log.Errorf("Get user relation err: %v", err)
-		return
+	if s.cache.Relation.IsExists(ctx, *req.BaseReq.UserId) != 0 {
+		user.IsFollow = s.cache.Relation.IsFollow(ctx, *req.BaseReq.UserId, user.Id)
+	} else {
+		user.IsFollow, err = s.dao.Relation.IsUserFollowed(ctx, *req.BaseReq.UserId, user.Id)
+		if err != nil {
+			Log.Errorf("Get user relation from db err: %v", err)
+			return
+		}
+		var action int64
+		if user.IsFollow {
+			action = 1
+		} else {
+			action = 0
+		}
+		s.cache.Relation.FollowAction(ctx, *req.BaseReq.UserId, user.Id, action)
 	}
 	resp.SetUser(&user)
 	return
